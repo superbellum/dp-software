572c0e93f72a69a94fbc50545d7c2eea412cab8f
Patrik Gugh <gugh.patrik@gmail.com>
2025-03-16 14:10:38 +0100

    [FE] Create criminals component

---
+++ dp-client/src/app/components/criminals/criminals.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criminals',
+  imports: [],
+  templateUrl: './criminals.component.html',
+  styleUrl: './criminals.component.css'
+})
+export class CriminalsComponent {
+
+}
